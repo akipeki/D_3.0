77fddd8a38ced04e9fabd7b0a27522f1057bb10e
Peki <112956568+akipeki@users.noreply.github.com>
2023-06-04 23:40:07 +0300

    sunday - last commit before bedtime

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -16,6 +16,11 @@ const [form, setForm] = useState({
 const [generatingImg, setGeneratingImg] = useState(false);
 const [loading, setLoading] = useState(false);
 
+
+const generateImage = () => {
+
+}
+
 const handleSubmit = () => {
 
 }
@@ -57,7 +62,58 @@ const handleSurpriseMe = () => {
       handleSurpriseMe={handleSurpriseMe}
       />
 
+<div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
+ {form.photo ? (
+  <img
+  src={form.photo}
+  alt={form.prompt}
+  className='w-full h-full object-contain'
+  />
+ ) : (
+  <img
+  src='{preview}'
+  alt='preview'
+  className='w-9/12 h-9/12 object-contain opacity-40'
+  />
+ )}
+
+ {generatingImg && (
+  <div className='absolute inset-0 z-0 flex
+  justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'> 
+    <Loader />
+  </div>
+ )}
+
+</div>
     </div>
+
+    <div className='mt-5 flex gap-5'>
+      <button
+      type='button'
+      onClick={generateImage}
+      className='text-white bg-green-700 font-medium
+      rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+
+      >
+        {generateImg ? 'Generating...' : 'Generate'}
+
+      </button>
+    </div>
+    <div className='mt-10'>
+      <p className='mt-2 text-[#665e75] text-[14px]'>
+        Once you have created the image you want, you can share it with others in the community.</p>
+    <button
+    type='submit'
+    className='mt-3 text-white bg-[#6469ff]
+    font-medium rounded-md text-sm w-full sm:w-auto
+    px-5 py-2.5 text-center'
+    >
+      {laoding ? 'Sharing...' : 'Share with the Community'}
+    </button>
+    
+    </div>
+
     </form>
 
     </section>
