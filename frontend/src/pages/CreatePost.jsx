8840f8da6617b890d89488b9dc3ad5ef56cbad0d
Peki <112956568+akipeki@users.noreply.github.com>
2023-06-07 00:31:04 +0300

    now it should work

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://dille.onrender.com/api/v1/dalle', {
+        const response = await fetch('https://dille.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -56,7 +56,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://dille.onrender.com/api/v1/post', {
+        const response = await fetch('https://dille.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -78,72 +78,72 @@ const CreatePost = () => {
   };
 
   return (
-    <section className="max-w-7xl mx-auto">
+    <section className='max-w-7xl mx-auto'>
       <div>
-        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
-        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through DALL-E AI and share it with the community</p>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
+        <p className='mt-2 text-[#666e75] text-[14px] max-w-[500px]'>Generate an imaginative image through DALL-E AI and share it with the community</p>
       </div>
 
-      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
-        <div className="flex flex-col gap-5">
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
           <FormField
-            labelName="Your Name"
-            type="text"
-            name="name"
-            placeholder="Ex., john doe"
+            labelName='Your Name'
+            type='text'
+            name='name'
+            placeholder='Ex., john doe'
             value={form.name}
             handleChange={handleChange}
           />
 
           <FormField
-            labelName="Prompt"
-            type="text"
-            name="prompt"
-            placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
+            labelName='Prompt'
+            type='text'
+            name='prompt'
+            placeholder='An Impressionist oil painting of sunflowers in a purple vase…'
             value={form.prompt}
             handleChange={handleChange}
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
 
-          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+          <div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
             { form.photo ? (
               <img
                 src={form.photo}
                 alt={form.prompt}
-                className="w-full h-full object-contain"
+                className='w-full h-full object-contain'
               />
             ) : (
               <img
                 src={preview}
-                alt="preview"
-                className="w-9/12 h-9/12 object-contain opacity-40"
+                alt='preview'
+                className='w-9/12 h-9/12 object-contain opacity-40'
               />
             )}
 
             {generatingImg && (
-              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+              <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
                 <Loader />
               </div>
             )}
           </div>
         </div>
 
-        <div className="mt-5 flex gap-5">
+        <div className='mt-5 flex gap-5'>
           <button
-            type="button"
+            type='button'
             onClick={generateImage}
-            className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            className=' text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
           >
             {generatingImg ? 'Generating...' : 'Generate'}
           </button>
         </div>
 
-        <div className="mt-10">
-          <p className="mt-2 text-[#666e75] text-[14px]">** Once you have created the image you want, you can share it with others in the community **</p>
+        <div className='mt-10'>
+          <p className='mt-2 text-[#666e75] text-[14px]'>** Once you have created the image you want, you can share it with others in the community **</p>
           <button
-            type="submit"
-            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            type='submit'
+            className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
           >
             {loading ? 'Sharing...' : 'Share with the Community'}
           </button>
