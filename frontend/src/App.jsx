13b659205ff7382bd4701f92bc6b8efc59a6b9d6
Peki <112956568+akipeki@users.noreply.github.com>
2023-06-06 19:01:20 +0300

    fixing typos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,8 @@ import { Home, CreatePost } from './pages';
 const App = () => {
   return (
  <BrowserRouter>
-  <header className='w-full flex justify-between items- bg-white
-  sm:px-8 px-4 py-4 border-b b[#e6ebf4]'>
+  <header className='w-full flex justify-between items-center bg-white
+  sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
     <Link to='/'>
 <img src={logo} alt="logo" className='w-28 object-contain'/>
     </Link>
@@ -15,7 +15,7 @@ const App = () => {
     bg-[#6469ff] text-white px-4 py-2 rounded-md'>Create</Link>
   </header>
   <main className='sm:p-8 px-4 py-8
-  w-full bg[#f9fafe] min-h-[calc(100vh-73px)]'>
+  w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
    <Routes>
    <Route path="/" element={<Home />} />
         <Route path="/create-post" element={<CreatePost />} />
