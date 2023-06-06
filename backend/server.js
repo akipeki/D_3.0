c32e5afa31579d802fdde40618f9c0c36ea87e74
Peki <112956568+akipeki@users.noreply.github.com>
2023-06-06 19:20:17 +0300

    fixing mongo etc

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.get('/', async (req, res) => {
 const startServer = async () => {
 
 try {
-connectDB(process.env.MONGODB_URL);
+connectDB(process.env.MONGO_URL);
 app.listen(8080, () => 
   console.log('Server running on port http://localhost:8080'));
 } catch (error) {
