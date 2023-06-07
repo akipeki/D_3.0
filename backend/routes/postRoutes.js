41726469f634dd47fabb1a4c383552d07fbe5ca9
Peki <112956568+akipeki@users.noreply.github.com>
2023-06-07 13:35:23 +0300

    adding end points to delete posts from postman

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -44,4 +44,23 @@ res.status(201).json({ success: true, data: newPost });
 
 });
 
+// DELETE A POST
+router.route('/:id').delete(async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+  
+      if (!post) {
+        return res.status(404).json({ success: false, message: 'Post not found' });
+      }
+  
+      // If the post was found, delete it
+      await post.remove();
+  
+      res.status(200).json({ success: true, message: 'Post deleted' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, message: 'Error deleting post' });
+    }
+  });
+
 export default router;
