2e0caca6842926d063d6882e80702e052128a705
Peki <112956568+akipeki@users.noreply.github.com>
2023-06-07 14:41:35 +0300

    font size in cards

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -13,7 +13,7 @@ const Card = ({ _id, name, prompt, photo }) => {
        />
       <div className='h-full group-hover:flex flex-col items-center justify-center 
    hidden absolute top-0 left-0 right-0 bg-white opacity-70 p-8 rounded-md'>
-    <p className='text-black text-md overflow-y-auto prompt mb-4 text-center'>{prompt}</p>
+    <p className='text-black text-sm overflow-y-auto prompt mb-4 text-center'>{prompt}</p>
     <div className='mt-5 flex justify-between items-center gap-2 w-full'>
         <div className='flex items-center gap-2'>
             <div className='w-7 h-7 rounded-full object-cover bg-green-700
