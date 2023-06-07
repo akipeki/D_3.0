171563affd55cf709a8113c6d664580a95d535b8
Peki <112956568+akipeki@users.noreply.github.com>
2023-06-07 15:50:23 +0300

    fixing card

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -11,8 +11,8 @@ const Card = ({ _id, name, prompt, photo }) => {
        src={photo}
        alt={prompt}
        />
-      <div className='h-full group-hover:flex flex-col items-center justify-center 
-   hidden absolute top-0 left-0 right-0 bg-white opacity-70 p-8 rounded-md'>
+     <div className='h-full group-hover:flex flex-col items-center justify-end 
+   hidden absolute top-0 left-0 right-0 bg-white opacity-70 p-8 rounded-md mb-3'>
     <p className='text-black text-sm overflow-y-auto prompt mb-4 text-center'>{prompt}</p>
     <div className='mt-5 flex justify-between items-center gap-2 w-full'>
         <div className='flex items-center gap-2'>
@@ -27,10 +27,9 @@ const Card = ({ _id, name, prompt, photo }) => {
             <img src={download} alt='download' className='w-6 h-6
             object-contain' />
         </button>
-            </div>
-          </div>
     </div>
-  )
+</div>
+
 }
 
 export default Card
