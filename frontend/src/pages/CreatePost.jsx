b08816ebec1ba34aea544b593b5b1c462774b8e0
Peki <112956568+akipeki@users.noreply.github.com>
2023-06-13 11:32:43 +0300

    removing some comments

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -5,13 +5,9 @@ import { preview } from '../assets';
 import { getRandomPrompt } from '../utils';
 import { FormField, Loader } from '../components';
 
-// Main component for creating a post
 const CreatePost = () => {
-  // useNavigate is a hook used for navigation between routes
   const navigate = useNavigate();
 
-  // useState is a hook to manage state in a functional component
-  // 'form' is an object containing form field values, 'setForm' is the function used to update those values
   const [form, setForm] = useState({
     name: '',
     prompt: '',
@@ -22,30 +18,46 @@ const CreatePost = () => {
     personDescription: '',
   });
 
+  const getGPT3Response = async (prompt) => {
+    try {
+      // Make a request to your GPT-3 endpoint, passing the prompt
+      const response = await fetch('https://dille.onrender.com/api/v1/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt }),
+      });
 
-  // State variables for tracking loading states and generated text
+      if (!response.ok) {
+        throw new Error('Failed to generate GPT-3 response');
+      }
+
+      // Assume that your GPT-3 API returns a JSON with the generated text in the 'text' field
+      const data = await response.json();
+      return data.text;
+    } catch (err) {
+      console.error(err);
+      return '';
+    }
+  };
+
+
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
   const [generatedText, setGeneratedText] = useState("");
 
-  // Event handler for form field changes
   const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
 
-  // Event handler for the "Surprise Me" button
-  // Generates a random prompt and updates the state accordingly
   const handleSurpriseMe = async () => {
     const randomPrompt = getRandomPrompt(form.prompt);
     setForm({ ...form, prompt: randomPrompt });
     await generateMessage(randomPrompt);
   };
 
-  // Function to generate message based on provided prompt
   const generateMessage = async (prompt) => {
-    // Checks if the prompt exists
     if (prompt) {
-      // Encloses async operations in try/catch for error handling
       try {
-        // Sends a POST request to API for generating message
         const response = await fetch('https://dille.onrender.com/api/v1/chat', {
           method: 'POST',
           headers: {
@@ -56,15 +68,12 @@ const CreatePost = () => {
           }),
         });
 
-        // If response from API is not okay, throws an error
         if (!response.ok) {
           const errData = await response.json();
           throw new Error(errData.message || `An error has occurred while generating message: ${response.status}`);
         }
 
-        // Parses the response to JSON
         const data = await response.json();
-        // Sets the generated message as the new prompt and updates the generated text
         setForm({ ...form, prompt: data });
         setGeneratedText(data);
       } catch (err) {
@@ -75,15 +84,10 @@ const CreatePost = () => {
     }
   };
 
-  // Function to generate image based on provided details
   const generateImage = async () => {
-    // Checks if all necessary details are provided
     if (form.prompt && form.gender && form.age) {
-      // Sets generatingImg to true while the image is being generated
-      setGeneratingImg(true);
-      // Encloses async operations in try/catch for error handling
       try {
-        // Sends a POST request to the DALL-E API for generating an image
+        setGeneratingImg(true);
         const response = await fetch('https://dille.onrender.com/api/v1/dalle', {
           method: 'POST',
           body: JSON.stringify({
@@ -94,15 +98,12 @@ const CreatePost = () => {
           },
         });
 
-        // If response from API is not okay, throws an error
         if (!response.ok) {
           const errData = await response.json();
           throw new Error(errData.message || `An error has occurred while generating an image: ${response.status}`);
         }
 
-        // Parses the response to JSON
         const data = await response.json();
-        // Updates the form data with the generated image
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
 
         await generateMessage(form.prompt);
@@ -110,7 +111,6 @@ const CreatePost = () => {
       } catch (err) {
         alert(`Fetch error: ${err.message}`);
       } finally {
-        // Sets generatingImg to false after the image is generated
         setGeneratingImg(false);
       }
     } else {
@@ -118,31 +118,24 @@ const CreatePost = () => {
     }
   };
 
-  // Event handler for form submission
   const handleSubmit = async (e) => {
-    // Prevents the default form submission action
     e.preventDefault();
 
-    // Checks if all necessary details are provided
     if (form.prompt && form.photo && form.gender && form.age && form.country) {
-      // Sets loading to true while the request is being made
       setLoading(true);
-      // Encloses async operations in try/catch for error handling
       try {
-        // Generates GPT-3 response for the prompt
+        // Generate GPT-3 response for the prompt
         const generatedText = await getGPT3Response(form.prompt);
 
-        // Sends a POST request to the server to create a new post
         const response = await fetch('https://dille.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          // Includes generatedText in the request body
+          // Include generatedText in the request body
           body: JSON.stringify({ ...form, generatedText }),
         });
 
-        // If response from server is not okay, throws an error
         if (!response.ok) {
           const errData = await response.json();
           throw new Error(errData.message || `An error has occurred while handling submit: ${response.status}`);
@@ -150,12 +143,10 @@ const CreatePost = () => {
 
         await response.json();
         alert('Success');
-        // Navigates to the homepage after successful post creation
         navigate('/');
       } catch (err) {
         alert(`Fetch error: ${err.message}`);
       } finally {
-        // Sets loading to false after the request is complete
         setLoading(false);
       }
     } else {
@@ -163,35 +154,7 @@ const CreatePost = () => {
     }
   };
 
-  // Async function to fetch generated text from GPT-3 model based on user's prompt
-  const getGPT3Response = async (prompt) => {
-    // A try/catch block is used for error handling
-    try {
-      // Fetch function is used to send a POST request to the GPT-3 API with prompt as input
-      const response = await fetch('https://dille.onrender.com/api/v1/chat', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ prompt }),
-      });
 
-      // If response is not ok, throw an error
-      if (!response.ok) {
-        throw new Error('Failed to generate GPT-3 response');
-      }
-
-      // Convert the response to JSON
-      const data = await response.json();
-      // Return the generated text from the response
-      return data.text;
-    } catch (err) {
-      console.error(err);
-      return '';
-    }
-  };
-
-  // Array of age options for the select field
   const ageOptions = [
     "1-3 years",
     "5-8 years",
@@ -210,12 +173,9 @@ const CreatePost = () => {
     "100+ years"
   ];
 
-  // Array of gender options for the select field
   const genderOptions = ['girl', 'boy', 'man', 'woman', 'child', 'adult'];
 
-  // JSX returned by the component
   return (
-    // Creates a section for the form
     <section className='max-w-7xl mx-auto'>
       <div>
         <h1 className='font-extrabold text-[#222328] text-[32px]'>Share your story</h1>
@@ -229,10 +189,8 @@ const CreatePost = () => {
         </p>
       </div>
 
-      {/* Starts the form with a submit event handler */}
       <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-5'>
-          {/* Form field for entering a name */}
           <FormField
             labelName='How would you like to be named here?'
             type='text'
@@ -242,7 +200,6 @@ const CreatePost = () => {
             handleChange={handleChange}
           />
 
-          {/* Form field for entering the apology */}
           <FormField
             labelName="I want to apologize that..."
             type='text'
@@ -254,9 +211,7 @@ const CreatePost = () => {
             handleSurpriseMe={handleSurpriseMe}
           />
 
-          {/* Flex container for gender and age fields */}
           <div className="flex justify-between gap-4">
-            {/* Form field for selecting gender */}
             <FormField
               style={{ flexGrow: 1, minWidth: 'calc(50% - 20px)' }}
               labelName="I want to apology a"
@@ -268,7 +223,7 @@ const CreatePost = () => {
               options={genderOptions}
             />
 
-            {/* Form field for selecting age */}
+
             <FormField
               style={{ flexGrow: 1, minWidth: 'calc(50% - 20px)' }}
               labelName="That person was"
@@ -279,49 +234,74 @@ const CreatePost = () => {
               handleChange={handleChange}
               options={ageOptions}
             />
+
           </div>
 
-          {/*  Form field for describing the person to apologize to */}
           <FormField
             labelName='How would you describe the person you want to apologize'
             type='text'
             name='personDescription'
-            placeholder='Ex., Cute and kind. They were always there to support me.'
+            placeholder='Ex., Cute and kind. Friendly eyes, curly hair. Always helping for others.'
             value={form.personDescription}
             handleChange={handleChange}
           />
 
-          {/*  Form field for entering the country */}
           <FormField
-            labelName='Where does this person live?'
+            labelName='City and country where did this happen?'
             type='text'
             name='country'
-            placeholder='Ex., France'
+            placeholder='Ex., Helsinki, Finland'
             value={form.country}
             handleChange={handleChange}
           />
 
-          {/*  Button to generate the anonymous image */}
-          <button className="mt-5 px-10 py-2 text-white bg-blue-500 rounded-lg focus:outline-none" onClick={generateImage}>
-            {generatingImg ? <Loader /> : 'Generate Image'}
+          <div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className='w-full h-full object-contain'
+              />
+            ) : (
+              <img
+                src={preview}
+                alt='preview'
+                className='w-9/12 h-9/12 object-contain opacity-40'
+              />
+            )}
+
+            {generatingImg && (
+              <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className='mt-5'>
+          <p className='mt-2 text-[#666e75] text-[14px]'>
+            {generatedText}
+          </p>
+          <button
+            type='button'
+            onClick={generateImage}
+            className=' text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+          >
+            {generatingImg ? 'Generating...' : 'Generate'}
           </button>
+        </div>
 
-          {/*  Conditionally rendered image or placeholder */}
-          {form.photo ? (
-            <img src={form.photo} alt="Generated avatar" className="w-full h-64 object-cover mt-5 rounded-lg" />
-          ) : (
-            <div className="w-full h-64 bg-gray-200 mt-5 rounded-lg flex justify-center items-center">
-              <img src={preview} alt="Placeholder avatar" />
-            </div>
-          )}
-
-          {/*  Button to submit the form */}
-          <button type="submit" className="mt-10 px-10 py-2 text-white bg-blue-500 rounded-lg focus:outline-none">
-            {loading ? <Loader /> : 'Publish my apology'}
+        <div className='mt-10'>
+          <p className='mt-2 text-[#666e75] text-[14px]'>** Once you have created the image you want, you can share it with others in the community **</p>
+          <button
+            type='submit'
+            className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+          >
+            {loading ? 'Sharing...' : 'Share with the Community'}
           </button>
         </div>
       </form>
-    </section>
+    </section >
   );
 };
 
