5db73dabfcc41e85eceab0f4f2f79781e0910383
Peki <112956568+akipeki@users.noreply.github.com>
2023-06-14 11:57:12 +0300

    updating right fetch address

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -34,7 +34,7 @@ export async function getMessages(value) {
     };
 
     try {
-        const response = await fetch("http://localhost:8000/completions", options);
+        const response = await fetch("https://api.openai.com/v1/chat/", options);
         const data = await response.json();
         return data.choices[0].message;
     } catch (error) {
