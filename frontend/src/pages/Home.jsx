28f75cb83ece297b5ffc814615700e6b7b8af8fd
Peki <112956568+akipeki@users.noreply.github.com>
2023-07-03 12:27:36 +0300

    removing LoaderHomePage from home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 // Importing necessary packages and components
 import React, { useEffect, useState } from 'react';
-import { Card, FormField, Loader, LoaderHomePage } from '../components';
+import { Card, FormField, Loader } from '../components';
 import { useMediaQuery } from 'react-responsive';
 
 // A function component that takes in data and title as props to render the cards or display a message
@@ -104,7 +104,6 @@ const Home = () => {
         onClick={() => setCurrentPage(number)}
         className={`px-2 py-1 mx-1 border border-none rounded-md focus:outline-none ${currentPage === number ? ' text-white bg-[#6469ff] ' : 'text-page-number'}`}
 
-
       >
         {number}
       </button>
@@ -134,89 +133,86 @@ const Home = () => {
   // Render the Home component
   return (
     <section className='max-w-7xl sm:mx-6 lg:mx-10 mx-auto'>
-      {homePageLoading && <div className="loader-container"><LoaderHomePage /></div>}
-      <div className={`content-container ${homePageLoading ? '' : 'visible'}`}>
 
-        <div>
-          <h1 className='font-extrabold text-[#1B2828] text-[32px]'>The Community Showcase</h1>
-          <p className='mt-4 text-[#666e75] text-[16px] max-w-[500px]'>These are intimate apologies from our community, highlighting the significance of speaking up. With cutting-edge technology (ChatGTP + DALL-E + code) we ensure your voice remains anonymous, yet visible.</p>
-          <p className='mt-4 text-[#666e75] text-[16px] max-w-[500px]'>If you can no longer apologise directly to the person you have hurt, may this platform provide solace as you navigate the path towards healing.</p>
-        </div>
+      <div>
+        <h1 className='font-extrabold text-[#1B2828] text-[32px]'>The Community Showcase</h1>
+        <p className='mt-4 text-[#666e75] text-[16px] max-w-[500px]'>These are intimate apologies from our community, highlighting the significance of speaking up. With cutting-edge technology (ChatGTP + DALL-E + code) we ensure your voice remains anonymous, yet visible.</p>
+        <p className='mt-4 text-[#666e75] text-[16px] max-w-[500px]'>If you can no longer apologise directly to the person you have hurt, may this platform provide solace as you navigate the path towards healing.</p>
+      </div>
 
-        <div className='mt-8 pt-2'>
-          {/* Search field */}
-          <FormField
-            labelName='Search posts'
-            type='text'
-            name='text'
-            placeholder='Search an apology...'
-            value={searchText}
-            handleChange={handleSearchChange}
-          />
-        </div>
+      <div className='mt-8 pt-2'>
+        {/* Search field */}
+        <FormField
+          labelName='Search posts'
+          type='text'
+          name='text'
+          placeholder='Search an apology...'
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
+      </div>
 
-        <div className='mt-10'>
-          {/* Show a loader when loading */}
-          {loading ? (
-            <div className='flex justify-center items-center'>
-              <Loader />
-            </div>
-          ) : (
-            <>
-              {/* Show a heading if search text is present */}
-              {searchText && (
-                <h2 className='font-medium text-[#666e75] text-xl mb-4'>
-                  Showing Results for <span className='text-[#1B2828]'>{searchText}</span>:
-                </h2>
+      <div className='mt-10'>
+        {/* Show a loader when loading */}
+        {loading ? (
+          <div className='flex justify-center items-center'>
+            <Loader />
+          </div>
+        ) : (
+          <>
+            {/* Show a heading if search text is present */}
+            {searchText && (
+              <h2 className='font-medium text-[#666e75] text-xl mb-4'>
+                Showing Results for <span className='text-[#1B2828]'>{searchText}</span>:
+              </h2>
+            )}
+            <div className='mt-12 sm:mt-20 mx-0 grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-2 xs:grid-cols-2 grid-cols-1 xl:gap-20 lg:gap-14 md:gap-8 sm:gap-14 xs:gap-14 xs:mx-8 sm:mx-12 md:mx-0'>
+
+              {isTabletOrMobileDevice && (
+                <div className='grid grid-cols-1 gap-0 mx-0'>
+                  <h6 className='mt-0 text-[16px] grid grid-cols-1 pt-0 font-medium text-grey-900'>Click images to read apologies</h6>
+                </div>
               )}
-              <div className='mt-12 sm:mt-20 mx-0 grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-2 xs:grid-cols-2 grid-cols-1 xl:gap-20 lg:gap-14 md:gap-8 sm:gap-14 xs:gap-14 xs:mx-8 sm:mx-12 md:mx-0'>
 
-                {isTabletOrMobileDevice && (
-                  <div className='grid grid-cols-1 gap-0 mx-0'>
-                    <h6 className='mt-0 text-[16px] grid grid-cols-1 pt-0 font-medium text-grey-900'>Click images to read apologies</h6>
-                  </div>
-                )}
+              {/* Render cards based on whether search text is present */}
+              {
+                searchText ? (
+                  <RenderCards
+                    data={searchedResults?.slice(indexOfFirstPost, indexOfLastPost) ?? []}
+                    title='No Search Results Found'
+                  />
+                ) : (
+                  <RenderCards
+                    data={allPosts?.slice(indexOfFirstPost, indexOfLastPost) ?? []}
+                    title='No Posts Yet'
+                  />
+                )
+              }
+            </div>
+            <div className='flex justify-center items-center mt-12'>
+              <button
+                style={{ fontSize: '20px ', color: '#666e75', }}
+                className={`px-2 py-1 mx-1 border border-none rounded-md focus:outline-none ${currentPage === 1 ? 'cursor-not-allowed' : ''}`}
+                onClick={() => setCurrentPage((page) => Math.max(page - 1, 1))}
+              >
+                {'<'}
+              </button>
 
-                {/* Render cards based on whether search text is present */}
-                {
-                  searchText ? (
-                    <RenderCards
-                      data={searchedResults?.slice(indexOfFirstPost, indexOfLastPost) ?? []}
-                      title='No Search Results Found'
-                    />
-                  ) : (
-                    <RenderCards
-                      data={allPosts?.slice(indexOfFirstPost, indexOfLastPost) ?? []}
-                      title='No Posts Yet'
-                    />
-                  )
-                }
-              </div>
-              <div className='flex justify-center items-center mt-12'>
-                <button
-                  style={{ fontSize: '20px ', color: '#666e75', }}
-                  className={`px-2 py-1 mx-1 border border-none rounded-md focus:outline-none ${currentPage === 1 ? 'cursor-not-allowed' : ''}`}
-                  onClick={() => setCurrentPage((page) => Math.max(page - 1, 1))}
-                >
-                  {'<'}
-                </button>
+              {searchText ? renderPageNumbers(searchedResults) : renderPageNumbers(allPosts)}
 
-                {searchText ? renderPageNumbers(searchedResults) : renderPageNumbers(allPosts)}
-
-                <button
-                  style={{ fontSize: '20px', color: '#666e75', }}
-                  className={`text-#666e75 px-2 py-1 mx-1 border border-none rounded-md focus:outline-none ${currentPage === (searchText ? Math.ceil(searchedResults.length / itemsPerPage) : Math.ceil(allPosts.length / itemsPerPage)) ? 'cursor-not-allowed' : ''}`}
-                  onClick={() => setCurrentPage((page) => Math.min(page + 1, (searchText ? Math.ceil(searchedResults.length / itemsPerPage) : Math.ceil(allPosts.length / itemsPerPage))))}
-                >
-                  {'>'}
-                </button>
-              </div>
+              <button
+                style={{ fontSize: '20px', color: '#666e75', }}
+                className={`text-#666e75 px-2 py-1 mx-1 border border-none rounded-md focus:outline-none ${currentPage === (searchText ? Math.ceil(searchedResults.length / itemsPerPage) : Math.ceil(allPosts.length / itemsPerPage)) ? 'cursor-not-allowed' : ''}`}
+                onClick={() => setCurrentPage((page) => Math.min(page + 1, (searchText ? Math.ceil(searchedResults.length / itemsPerPage) : Math.ceil(allPosts.length / itemsPerPage))))}
+              >
+                {'>'}
+              </button>
+            </div>
 
 
-            </>
+          </>
 
-          )}
-        </div>
+        )}
       </div>
     </section>
   );
