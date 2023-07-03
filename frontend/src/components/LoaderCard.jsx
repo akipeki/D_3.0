282eafa19a10ea0480516c823f6ca08b0c6d87d7
Peki <112956568+akipeki@users.noreply.github.com>
2023-07-03 20:10:50 +0300

    Adding Loader cards

---
+++ frontend/src/components/LoaderCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LoaderCard.css';
+
+const LoaderCard = () => {
+    return (
+        <div className="loader-card-container">
+            <p className="loader_card">
+                .
+            </p>
+        </div>
+    );
+};
+
+export default LoaderCard;
